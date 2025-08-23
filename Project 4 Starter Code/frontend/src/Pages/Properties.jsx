1b50739f14efad57c6b9ca3be6af9869d68a10ad
MomenAbdullah <momenahmad.abdullah@gmail.com>
2025-08-23 16:11:38 +0300

    add log out

--- Project 4 Starter Code/frontend/src/Pages/Properties.jsx
+++ Project 4 Starter Code/frontend/src/Pages/Properties.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logOut } from "../redux/slices/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const PropertiesPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    dispatch(logOut());
+    navigate("/");
+  };
+
   return (
     <div>
-      <h1>Properties Page</h1>
-      <p>WELCOME</p>
+      <h1>Welcome</h1>
+      <button onClick={handleLogOut}>Logout</button>
     </div>
   );
 };
