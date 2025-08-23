aee2182968b27d3c96f77d29e04c7976eab86902
MomenAbdullah <momenahmad.abdullah@gmail.com>
2025-08-23 13:40:00 +0300

    home page

--- Project 4 Starter Code/frontend/src/App.jsx
+++ Project 4 Starter Code/frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/LogIn";  
-
+import PropertiesPage from "./Pages/Properties";
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<LoginPage />} />
-        
+        <Route path="/properties" element={<PropertiesPage />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ Project 4 Starter Code/frontend/src/Pages/Properties.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PropertiesPage = () => {
+  return (
+    <div>
+      <h1>Properties Page</h1>
+      <p>WELCOME</p>
+    </div>
+  );
+};
+
+export default PropertiesPage;
